269552acc590790634de50a6fc20410dd0abbf5d
Balram-Choudhary1 <balramchoudhary1608@gmail.com>
2024-12-10 18:26:38 +0530

    dddd

---
+++ src/screens/first.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const first = () => {
+  return (
+    <View>
+      <Text>first</Text>
+    </View>
+  )
+}
+
+export default first
+
+const styles = StyleSheet.create({})
